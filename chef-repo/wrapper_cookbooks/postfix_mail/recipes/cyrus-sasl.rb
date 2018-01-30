21c4f7d27e4b45c4d78140fa940f4c67f7a81e59
tyronemsaunders <tyrone.saunders@yahoo.com>
2018-01-29 23:00:28 -0600

    Chef Client finished

--- chef-repo/wrapper_cookbooks/postfix_mail/recipes/cyrus-sasl.rb
+++ chef-repo/wrapper_cookbooks/postfix_mail/recipes/cyrus-sasl.rb
@@ -10,39 +10,39 @@
 cyrus_sasl = node['secrets']['mail'][node.chef_environment]['cyrus_sasl']
 
 # Install Cyrus SASL
-package "Install Cyrus SASL" do
+package 'Install Cyrus SASL' do
   package_name ['expect', 'sasl2-bin', 'libsasl2-modules']
   action :upgrade
 end
 
 # script to generate sasl user and password
-cookbook_file "/etc/postfix/saslpasswd2.sh" do
-  source "saslpasswd2.sh"
+cookbook_file '/etc/postfix/saslpasswd2.sh' do
+  source 'saslpasswd2.sh'
   mode '0755'
 end
 
 # create the user name / password database file in the default location /etc/sasldb2
-execute "create sasl username and password" do
+execute 'create sasl username and password' do
   live_stream true
-  command "/etc/postfix/saslpasswd2.sh #{node['deploy']['mail'][node.chef_environment][0]['domain']} #{cyrus_sasl['username']} #{cyrus_sasl['password']}"
+  command "sudo /etc/postfix/saslpasswd2.sh #{node['deploy']['mail'][node.chef_environment][0]['domain']} #{cyrus_sasl['username']} #{cyrus_sasl['password']}"
 end
 
 # adjust permissions for /etc/sasldb2
-file "/etc/sasldb2" do
+file '/etc/sasldb2' do
   mode '0400'
   owner 'postfix'
 end
 
 # create smtpd.conf for authentication instructions
 # tell Cyrus SASL to use the file-based database to authenticate
-cookbook_file "/etc/postfix/sasl/smtpd.conf" do
-  source "smtpd.conf"
+cookbook_file '/etc/postfix/sasl/smtpd.conf' do
+  source 'smtpd.conf'
   group 'postfix'
   mode '0755'
 end
 
 # restart postfix
-bash "restart postfix" do
+bash 'restart postfix' do
   live_stream true
   code <<-EOH
     postfix stop
